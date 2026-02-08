6cb7585e02df8143a2ade340362256270ffc7142
Ingo Fruend <github@ingofruend.net>
2026-02-08 23:00:27 +0100

    Attempt share

--- src/utils/downloadObject.ts
+++ src/utils/downloadObject.ts
@@ -1,14 +1,31 @@
 import type { Transaction_t } from "../schema";
 
 export const downloadTransaction = (transaction: Transaction_t ) => {
-    const jsonString = JSON.stringify(transaction);
-    const blob = new Blob([jsonString], {type: "application/json"});
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = `transaction_${transaction.date.toISOString().split('T')[0]}_${crypto.randomUUID()}.json`
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(url);
+  const filename = `transaction_${transaction.date.toISOString().split('T')[0]}_${crypto.randomUUID()}.json`;
+  const jsonString = JSON.stringify(transaction);
+  const blob = new Blob([jsonString], {type: "application/json"});
+  const file = new File([blob], filename, {type: 'application/json' });
+
+  if (navigator.share && navigator.canShare({ files: [file] })) {
+    navigator.share({
+      files: [file],
+      title: 'Transaction'
+    }).catch((err) => {
+      downloadFile(blob, filename);
+    });
+  } else {
+    downloadFile(blob, filename);
+  }
+}
+
+
+const downloadFile = (blob: Blob, filename: string) => {
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(url);
 }
